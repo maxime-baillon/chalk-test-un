bfff1351b6205832c33e46e6d2d66f66046daa32
maxime baillon <baillonmaximebaillon@gmail.com>
2021-09-29 10:27:29 +0200

    git@github.com:maxime-baillon/chalk-test1.git

---
+++ index.js
@@ -0,0 +1,10 @@
+const chalk = require ("chalk");
+
+let testUN = "pif";
+let testDeux = "paf";
+let testTrois = "pouf";
+
+console.log(chalk.blue(testUN));
+console.log(chalk.white(testeDeux));
+console.log(chalk.red(testTrois));
+
